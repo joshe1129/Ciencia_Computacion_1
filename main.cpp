40e105d45dabef306c384bae4ddfa47662b7a1d0
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-24 10:53:30 -0500

    24/03/2017

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    /*
+    float pi = 3.1416;
+    cout<<"Area de un circulo";
+    cout<<"ingrese radio:";
+    cin>>x;
+    float area = pi * (pow(x,2));
+    cout << "EL area del circulo es: " << area << endl;
+    cout<<"Area de un cuadrado"<< endl;
+    cout<<"ingrese lado:";
+    cin>>x;
+    area = (x*x);
+    cout << "EL area del cuadrado es: " << area << endl;
+    cout<<"Area de un rectangulo"<< endl;
+    cout<<"ingrese base:";
+    cin>>x;
+    cout<<"ingrese altura:"<< endl;
+    cin>>y;
+    area = (x*y);
+    cout << "EL area del cuadrado es: " << area << endl;
+
+    ********************************
+    cout<<"ingrese x:";
+    cin>>x;
+    cout<<"ingrese y:";
+    cin>>y;
+    int var;
+    var = y;
+    y = x;
+    x = var;
+    cout<< "x = " << x << endl;
+    cout<< "y = " << y << endl;
+    *******************************
+    */
+    cout<<"ingrese x:";
+    cin>>x;
+    cout<<"ingrese y:";
+    cin>>y;
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    cout<< "x = " << x << endl;
+    cout<< "y = " << y << endl;
+}
