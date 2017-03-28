8582fccada10d5bbc86518981df9bcedadebdbec
joshe1129 <franshesco_save@outlook.com>
2017-03-27 21:53:59 -0500

    manipular coordenadas

--- Nave_ProyectoFinal/main.cpp
+++ Nave_ProyectoFinal/main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
+#include <stdio.h>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    printf("*");
+    HANDLE hCon;
+    hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD dwPos;
+    dwPos.X = 4;
+    dwPos.Y = 5;
+    SetConsoleCursorPosition(hCon, dwPos);
+    printf("*");
+    dwPos.X = 20;
+    dwPos.Y = 16;
+    SetConsoleCursorPosition(hCon, dwPos);
+    printf("*");
+
     return 0;
+
 }
