7b98140828daf3f4e843e1c748f5687c02062bc7
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2017-03-28 10:43:42 -0500

    operadores equivalencias, ternario y arrays

--- 28_03_2017/main.cpp
+++ 28_03_2017/main.cpp
@@ -23,12 +23,47 @@ int main()
     i/=2;
     cout << i << endl;
 
-    *******************operador ternario ************************* */
+    *******************operador ternario *************************
     int a,b,c;
     cout<<"ingrese a: " << endl;
     cin>>a;
     cout<<"ingrese b: " << endl;
     cin>>b;
     cout<<"El numero mayor es:" << (c=(a>b)?a:b) << endl; // if(a>b) c=a; else c = b;
+    *********************ARREGLOS******************************************
+    int arreglo[5];
+    int a;
+    cout<<"ingrese un entero: " << endl;
+    cin>>a;
+    arreglo[0]=a;
+    cout<<"ingrese un entero: " << endl;
+    cin>>a;
+    arreglo[1]=a;
+    cout<<"ingrese un entero: " << endl;
+    cin>>a;
+    arreglo[2]=a;
+    cout<<"ingrese un entero: " << endl;
+    cin>>a;
+    arreglo[3]=a;
+    cout<<"ingrese un entero: " << endl;
+    cin>>a;
+    arreglo[4]=a;
+    cout<< "el arreglo es: "<<"["<<arreglo[0]<<"]"<<"["<<arreglo[1]<<"]"<<"["<<arreglo[2]<<"]"<<"["<<arreglo[3]<<"]"<<"["<<arreglo[4]<<"]"<< endl;
+    **************************************************************************/
+    int score[6];
+    int c = 5;
+    score[0] = c++;
+    score[1] = c++;
+    score[2] = c++;
+    score[3] = c++;
+    score[4] = c++;
+    score[5] = c++;
+
+    cout<< score[0] << endl;
+    cout<< score[1] << endl;
+    cout<< score[2] << endl;
+    cout<< score[3] << endl;
+    cout<< score[4] << endl;
+    cout<< score[5] << endl;
 
 }
