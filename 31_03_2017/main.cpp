98975c1f097b1b968a8f90d9044cd59b88a3be8d
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-31 11:01:10 -0500

    funciones recurcivas

--- 31_03_2017/main.cpp
+++ 31_03_2017/main.cpp
@@ -4,6 +4,52 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    /* *************************************
+    int score[4];
+    int i=1,var;
+    cout<<"ingrese el valor del arreglo "<<i<<endl;
+    cin>>score[0];
+    i++;
+    cout<<"ingrese el valor del arreglo "<<i<<endl;
+    cin>>score[1];
+    i++;
+    cout<<"ingrese el valor del arreglo "<<i<<endl;
+    cin>>score[2];
+    i++;
+    cout<<"ingrese el valor del arreglo "<<i<<endl;
+    cin>>score[3];
+    var=score[0];
+    score[0]=score[3];
+    score[3]=var;
+    var=score[1];
+    score[1]=score[2];
+    score[2]=var;
+    cout<< "******" << endl;
+    cout<< score[0] << endl;
+    cout<< score[1] << endl;
+    cout<< score[2] << endl;
+    cout<< score[3] << endl;
+    ********************************* */
+    int tam,i,var, temp;
+    cout<<"ingrese tamaño del arreglo: ";
+    cin>>tam;
+    int arreglo[tam];
+    for(i=0;i<tam;i++){
+        cout<<"ingrese el valor del arreglo "<<i<<endl;
+        cin>>arreglo[i];
+    }
+    cout<< "******" << endl;
+    temp=tam-1;
+    for(i=0;i<tam/2;i++){
+        var=arreglo[i];
+        arreglo[i]=arreglo[temp];
+        arreglo[temp]=var;
+        temp--;
+    }
+    cout<< "******" << endl;
+    for(i=0;i<tam;i++){
+        cout<<arreglo[i]<<endl;
+    }
+
+
 }
